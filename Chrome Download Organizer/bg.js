eb883c146fcda2e65a732dea801647d8530bfdd8
Aidan Daly-Jensen <aidandj@gmail.com>
2013-06-09 23:13:41 -0700

    Updated script because it branched from original source

--- Chrome Download Organizer/bg.js
+++ Chrome Download Organizer/bg.js
@@ -1,16 +1,8 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Force all downloads to overwrite any existing files instead of inserting
-// ' (1)', ' (2)', etc.
-
 chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
     var current = item.url;
     for (var i = 0; i < localStorage.length; ++i){
         if (current.search(localStorage[localStorage.key(i)])){
             suggest({filename: localStorage[localStorage.key(i)] + '/' + item.filename, overwrite: false});
-                                                   }
-                                                   }
-
+        }
+    }
 });
