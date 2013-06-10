2d1cfcc655238c355686ae58ac6665d74b2e64d5
Aidan Daly-Jensen <aidandj@gmail.com>
2013-06-10 01:39:58 -0700

    Fixed a searching problem that would cause all downloads to be moved to a folder

--- Chrome Download Organizer/bg.js
+++ Chrome Download Organizer/bg.js
@@ -1,7 +1,7 @@
 chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
     var current = item.url;
     for (var i = 0; i < localStorage.length; ++i){
-        if (current.search(localStorage[localStorage.key(i)])){
+        if (current.indexOf(localStorage.key(i)) !== -1){
             suggest({filename: localStorage[localStorage.key(i)] + '/' + item.filename, overwrite: false});
         }
     }
