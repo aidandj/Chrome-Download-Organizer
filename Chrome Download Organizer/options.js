c20d3a385dae75eaf7aa984d034507991abb4d42
Aidan Daly-Jensen <aidandj@gmail.com>
2013-06-15 13:45:29 -0700

    Added version tracking for sure
    v0.1.5.8

--- Chrome Download Organizer/options.js
+++ Chrome Download Organizer/options.js
@@ -4,7 +4,7 @@ var _gaCode = 'UA-41728018-1';
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', _gaCode]);
 _gaq.push(['_setCustomVar',
-           1,
+           4,
            'Version: ',
            getVersion()
            ]);
